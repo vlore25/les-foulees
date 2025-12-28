41a1759249df79515bbc99cae1d40d38cf1511d4
vlore25 <victor.lorep@gmail.com>
2025-12-28 15:10:50 +0100

    Fix prisma build

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,6 +1,13 @@
 import 'dotenv/config'
 import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from './app/generated/prisma/client'
+import { defineConfig } from '@prisma/config';
+
+export default defineConfig({
+  datasource: {
+    url: process.env.DATABASE_URL,
+  },
+});
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
 const prisma = new PrismaClient({ adapter })
