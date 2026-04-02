9b18aa8d8b198eb067cd65e47a1586a591651b22
vlore25 <victor.lorep@gmail.com>
2026-04-03 01:58:13 +0200

    Fix upload folder in dockerIgnore

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone', // AJOUTEZ CECI : Optimise pour Docker
   experimental: {
     serverActions: {
       bodySizeLimit: '5mb',
