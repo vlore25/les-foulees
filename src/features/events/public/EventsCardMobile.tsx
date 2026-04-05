3543e67041f5859609d7782e24df5cdbed600981
vlore25 <victor.lorep@gmail.com>
2026-04-06 01:14:33 +0200

    Fix path

--- src/features/events/public/EventsCardMobile.tsx
+++ src/features/events/public/EventsCardMobile.tsx
@@ -24,12 +24,16 @@ export default function EventsCardMobile({ events }: EventsProps) {
                     <Card key={event.id} className='border-none gap-1 shadow-md hover:shadow-lg transition-shadow duration-300 pt-0 overflow-hidden flex flex-col h-full'>
                         <Link href={`/espace-membre/evenements/${event.id}`} className="block cursor-pointer hover:opacity-95 transition-opacity">
                             <div className="relative w-full h-48">
-                                <Image
+                                <img>
                                     src={`http://82.165.134.12${event.imgUrl}` || '/images/login-hero.jpg'}
                                     alt={event.title}
+                                </img>
+                                {/*<Image
+                                    
+                                    
                                     fill={true}
                                     className="object-cover"
-                                />
+                                />*/}
                             </div>
                             <CardHeader className='pt-4 px-4 pb-2'>
                                 <div className="flex flex-row justify-between content-center items-center">
