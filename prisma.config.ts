de250423f02e8f62bbc4aec54bda385524b0c14d
vlore25 <victor.lorep@gmail.com>
2026-06-16 13:54:39 +0200

    Database env prefix fix 2

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,8 +3,6 @@ import { defineConfig, env } from '@prisma/config';
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
-  
-  // Tes paramètres de migrations et seed que j'avais zappés
   migrations: {
     path: "prisma/migrations",
     seed: "bunx tsx prisma/seed.ts" 
