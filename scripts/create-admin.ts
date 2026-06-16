3798b634a509345675ce6a39fb0e4ae8a8239fca
vlore25 <victor.lorep@gmail.com>
2026-06-16 15:18:36 +0200

    Database env prefix fix 4

--- scripts/create-admin.ts
+++ scripts/create-admin.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from '../prisma/generated/client'
 import * as bcrypt from 'bcrypt';
 
 const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!,
+  connectionString: process.env.POSTGRES_DATABASE_URL!,
 });
 const prisma = new PrismaClient({ adapter })
 
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,7 +3,7 @@ import { PrismaPg } from '@prisma/adapter-pg';
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!,
+  connectionString: process.env.POSTGRES_DATABASE_URL!,
 });
 
 export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter })
